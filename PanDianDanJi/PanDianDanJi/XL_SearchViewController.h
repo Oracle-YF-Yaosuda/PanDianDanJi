87cb5855e4189035c2691335718bb0161736d53c
ljf651218196 <newmac@newmacdeiMac.local>
2016-09-19 17:29:10 +0800

    1

---
+++ PanDianDanJi/PanDianDanJi/XL_SearchViewController.h
@@ -0,0 +1,14 @@
+//
+//  XL_SearchViewController.h
+//  PanDianDanJi
+//
+//  Created by newmac on 16/9/19.
+//  Copyright © 2016年 BinXiaolang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XL_SearchViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITableView *table;
+
+@end
---
+++ PanDianDanJi/PanDianDanJi/XL_SearchViewController.m
@@ -0,0 +1,37 @@
+//
+//  XL_SearchViewController.m
+//  PanDianDanJi
+//
+//  Created by newmac on 16/9/19.
+//  Copyright © 2016年 BinXiaolang. All rights reserved.
+//
+
+#import "XL_SearchViewController.h"
+
+@interface XL_SearchViewController ()
+
+@end
+
+@implementation XL_SearchViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
